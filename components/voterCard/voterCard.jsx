d3abe6f718e49d0c3f0685ec737c3a00d46684db
Rahul Singh <96829896+rahul12st@users.noreply.github.com>
2024-07-24 02:55:58 +0530

    Update voterCard.jsx

--- components/voterCard/voterCard.jsx
+++ components/voterCard/voterCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 import Style from "../card/card.module.css";
-import image from "../../candidate.png";
+
 import voterCardStyle from "./voterCard.module.css";
 
 const voterCard = ({ voterArray }) => {
